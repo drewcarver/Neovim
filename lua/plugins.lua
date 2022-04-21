f394f6809f02244730f9fa3163fdb73755c99299
Andrew Carver <drew.carver@outlook.com>
2022-04-20 23:01:48 -0400

    add status bar

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,4 +29,8 @@ return require('packer').startup(function()
   }
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use {
+  'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
--- lua/theme.lua
+++ lua/theme.lua
@@ -1,3 +1,4 @@
 vim.cmd[[colorscheme embark]]
 vim.cmd[[set termguicolors]]
 vim.cmd[[set rnu]]
+vim.cmd[[set nowrap]]
